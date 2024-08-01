3079928436889d4be9a0c30d4faa02efeee6b846
teedinh <tien.dq@cxptek.com>
2024-08-01 17:37:54 +0700

    Chore: Init project

---
+++ src/main/java/com/ops/OpsBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsBillingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsBillingServiceApplication.class, args);
+	}
+
+}
